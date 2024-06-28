69985434c89a67584382dcaf1adc4b29dca8a620
Liyo08 <liyotom04@gmail.com>
2024-06-28 22:27:11 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import AddProduct from './components/AddProduct';
 import SearchProduct from './components/SearchProduct';
 import ViewProduct from './components/ViewProduct';
+import ProductNavbar from './components/ProductNavbar';
 
 function App() {
   return (
     <div>
+      <ProductNavbar/>
       <AddProduct/>
       <SearchProduct/>
       <ViewProduct/>
